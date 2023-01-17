260000ad8f8f69739bb37a37ac8ec655d9cdf545
Linus Foxell <linusfoxell@hotmail.com>
2023-01-17 18:28:42 +0100

    fix: styling

--- components/frontpageCard.tsx
+++ components/frontpageCard.tsx
@@ -6,7 +6,7 @@ const FrontpageCard: FC<any> = ({ item }) => {
   return (
     <div className="px-5">
       <div
-        className={`max-w-[85rem] mx-auto bg-slate-600 mb-10 h-[600px] rounded-lg text-white`}
+        className={`group max-w-[85rem] mx-auto bg-slate-600 mb-10 h-[600px] rounded-lg text-white hover:opacity-95 transition shadow-md`}
         style={{ backgroundColor: `${item.colorHex}` }}
       >
         <Link
