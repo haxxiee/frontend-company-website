a4d030b17633c37e89e2393ae239470fe4e8bf7e
Linus Foxell <linusfoxell@hotmail.com>
2023-01-18 10:45:27 +0100

    add: about page + links in header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,17 +1,31 @@
+import Link from "next/link";
 import { FC } from "react";
 
 const Header: FC = () => {
   return (
     <div className="flex justify-between items-center max-w-[76rem] mx-auto p-5">
-      <div className="font-bold text-4xl">WebApp.</div>
+      <Link href={"/"}>
+        <div className="font-bold text-4xl">WebApp.</div>
+      </Link>
       <div>
         <ul className="flex items-center gap-6 text-xl font-medium text-gray-600">
-          <li>Case Studies</li>
-          <li>About</li>
-          <li>Career</li>
-          <button className="bg-black px-8 py-4 rounded-md text-base font-semibold text-white">
-            Let&apos;s Talk
-          </button>
+          <Link href={"/case-studies"}>
+            <li>Case Studies</li>
+          </Link>
+          <Link href={"/about"}>
+            <li>About</li>
+          </Link>
+          <Link href={"/blog"}>
+            <li>Blog</li>
+          </Link>
+          <Link href={"/careers"}>
+            <li>Career</li>
+          </Link>
+          <Link href={"/contact"}>
+            <button className="bg-black px-8 py-4 rounded-md text-base font-semibold text-white">
+              Let&apos;s Talk
+            </button>
+          </Link>
         </ul>
       </div>
     </div>
