2a6183da83e269aaaad4cb6400dfef9efbd09f94
Linus Foxell <linusfoxell@hotmail.com>
2023-01-18 14:43:19 +0100

    add: case study card

---
+++ components/casestudyCard.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FC } from "react";
+
+const CasestudyCard: FC<any> = ({ item }) => {
+  return (
+    <div
+      className="group flex flex-col justify-center max-w-xl text-white rounded-md"
+      style={{ backgroundColor: `${item.colorHex}` }}
+    >
+      <Link
+        href={{
+          pathname: "/case-studies/[slug]",
+          query: { slug: item.slug },
+        }}
+      >
+        <div>
+          <div>
+            <Image
+              src={item.mainImage.data.attributes.url}
+              alt="Image of company product"
+              width={500}
+              height={500}
+              className="w-auto h-auto rounded-md"
+              priority
+            />
+          </div>
+          <div className="m-10">
+            <h1 className="font-bold text-4xl">{item.companyName}</h1>
+            <p>{item.companyDescription}</p>
+            <div className="w-16 h-[1px] my-7 bg-white"></div>
+            <div className="flex items-center gap-2">
+              <p className="font-semibold">View case study</p>
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="opacity-25 group-hover:opacity-100 transition-all"
+              >
+                <path
+                  d="M12.0519 14.8285L13.4661 16.2427L17.7087 12L13.4661 7.7574L12.0519 9.17161L13.8803 11H6.34318V13H13.8803L12.0519 14.8285Z"
+                  fill="currentColor"
+                />
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M1 19C1 21.2091 2.79086 23 5 23H19C21.2091 23 23 21.2091 23 19V5C23 2.79086 21.2091 1 19 1H5C2.79086 1 1 2.79086 1 5V19ZM5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default CasestudyCard;
--- pages/case-studies/index.tsx
+++ pages/case-studies/index.tsx
@@ -1,7 +1,41 @@
-export default function Page({ post }: any) {
+import axios from "axios";
+import CasestudyCard from "../../components/casestudyCard";
+
+export default function Page({ data }: any) {
   return (
-    <div>
-      <h1>CASE STUDIES PAGE</h1>
+    <div className="flex flex-col justify-center items-center max-w-7xl mx-auto">
+      <div className="py-28 px-10 mx-auto max-w-4xl">
+        <h1 className="text-5xl font-bold my-3">
+          Launching products that scale
+        </h1>
+        <p className="text-2xl text-gray-500 font-normal">
+          WebApp partners with clients to design and develop custom mobile apps,
+          websites, platforms and experiences. We work with an array of clients,
+          from global brands and media properties to innovative startups and
+          funded ventures.
+        </p>
+      </div>
+      <div className="flex flex-row flex-wrap items-center justify-center gap-10">
+        {data.map((item: any) => {
+          return <CasestudyCard key={item.slug} item={item} />;
+        })}
+      </div>
     </div>
   );
+}
+
+export async function getStaticProps(context: any) {
+  const response = await axios.get(
+    `http://0.0.0.0:1337/api/case-studies?populate=*`
+  );
+
+  const rawData = response.data.data;
+  const data = rawData.map((obj: any) => {
+    return obj.attributes;
+  });
+  return {
+    props: {
+      data,
+    },
+  };
 }
